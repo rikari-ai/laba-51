962f0525cd0c30a91851bbcf6f9b72a5e2e08b56
kristina <rkou12344@gmail.com>
2024-10-28 09:10:38 +0300

    Добавлен код про время

---
+++ neznay.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def get_date(prompt):
+    while True:
+        user_input = input(prompt)
+        try:
+            date = datetime.strptime(user_input, "%Y-%m-%d %H:%M")
+            return date
+        except ValueError:
+            print("Неверный формат. Пожалуйста, введите дату в формате ГГГГ-ММ-ДД ЧЧ:ММ.")
+
+print("Введите дату отправления (ГГГГ-ММ-ДД ЧЧ:ММ):")
+departure = get_date("Отправление: ")
+
+print("Введите дату прибытия (ГГГГ-ММ-ДД ЧЧ:ММ):")
+arrival = get_date("Прибытие: ")
+
+if arrival < departure:
+    print("Дата прибытия не может быть раньше даты отправления.")
+else:
+    travel_time = arrival - departure
+    print(f"Время в пути: {travel_time}")
